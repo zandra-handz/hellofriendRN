2f0ef53d56def96644ca651118c6938798b87081
zandra_handz <taylorcolegrove@gmail.com>
2025-05-09 23:26:02 -0400

    added some bottom height to category buttons

--- app/components/moments/ContentMomentFocus.jsx
+++ app/components/moments/ContentMomentFocus.jsx
@@ -208,7 +208,7 @@ const ContentMomentFocus = ({
         <View
           style={{
             position: "absolute",
-            bottom: 0,
+            bottom: 32,
             left: 0,
             right: 0,
             width: "100%",
