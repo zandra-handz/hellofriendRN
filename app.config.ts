9a57effbcd03c55069b7dd8b24ddb03561e48771
zandra_handz <taylorcolegrove@gmail.com>
2026-04-19 21:42:41 -0400

    oop forgot to increment build number

--- app.config.ts
+++ app.config.ts
@@ -16,7 +16,7 @@ const config = ({ config: baseConfig }: ConfigContext): ExpoConfig => {
       ios: {
         jsEngine: "hermes",  
         bundleIdentifier: 'com.badrainbowz.sampleproject',
-        buildNumber: '8', 
+        buildNumber: '9', 
         supportsTablet: true,
         googleServicesFile: './GoogleService-Info.plist',
         infoPlist: {
