540078a053f06275e27d779fb757a20982a4339d
zandra_handz <taylorcolegrove@gmail.com>
2026-05-06 19:37:04 -0400

    incremented build number

--- app.config.ts
+++ app.config.ts
@@ -16,7 +16,7 @@ const config = ({ config: baseConfig }: ConfigContext): ExpoConfig => {
       ios: {
         jsEngine: "hermes",  
         bundleIdentifier: 'com.badrainbowz.sampleproject',
-        buildNumber: '12', 
+        buildNumber: '44', 
         supportsTablet: true,
         googleServicesFile: './GoogleService-Info.plist',
         infoPlist: {
